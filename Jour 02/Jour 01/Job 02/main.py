86161b3d7ae0136b305d1c43ffc117076b2453b6
Lawrie FALCAO <130457520+lawrie-falcao@users.noreply.github.com>
2023-04-25 14:22:23 +0200

    Delete Jour 02/Jour 01 directory

--- Jour 02/Jour 01/Job 02/main.py
+++
@@ -1,7 +0,0 @@
-class Operation:
-    def __init__(self, nombre1=12, nombre2=3):
-        self.nombre1 = 12
-        self.nombre2 = 3
-operation = Operation()
-print(operation.nombre1)
-print(operation.nombre2)
